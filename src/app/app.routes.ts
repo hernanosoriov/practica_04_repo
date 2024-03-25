c2aa8c68c34cf4a54244b176586127ef9a1d5533
hernan <hosorio2@CPU-6510.mef.gob.pe>
2024-03-25 16:18:48 -0500

    version01

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Componente02Component } from './modulo02/componente02/componente02.component';
+import { Componente03Component } from './modulo03/componente03/componente03.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/componente2', pathMatch: 'full' },
+    {path:'componente2',component: Componente02Component},
+    {path:'componente3',component: Componente03Component}
+
+];
